ef1ac02f96cfee17cb09f42ca8ed338feff4fe0e
kameron <khr675@gmail.com>
2023-02-25 11:29:33 -0600

    added login/signup buttons

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,11 +1,42 @@
 <template>
+  <div class="welcome">
+    {{ welcomeTxt }}
+  </div>
   <div>
-    Home
+    <div class="loginButton" @click="onLoginClicked">
+      {{ loginTxt }}
+    </div>
+    <div class="signupButton" @click="onSignUpClicked">
+      {{ signupTxt }}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HomeView',
+  data() {
+    return {
+      welcomeTxt: "Welcome to the Bee's Kneez",
+      loginTxt: "Login",
+      signupTxt: "Sign Up",
+    }
+  },
+  methods: {
+    onLoginClicked() {
+      console.log("Login was clicked!")
+    },
+    onSignUpClicked() {
+      console.log("Signup was clicked!")
+    }
+  }
 }
 </script>
+
+<style>
+.welcome {
+  background-color: lightyellow;
+  font-size: 16px;
+  font-family: sans-serif;
+}
+</style>
