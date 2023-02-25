0e07b5f05ca1c063ea06588e98ac3a3df5a36118
Runavv <iskandav@myumanitoba.ca>
2023-02-25 10:51:43 -0600

    Lists page not working

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView'
-
+import ViewToDos from '../views/ViewToDos'
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
   },
+  {
+    path: '/',
+    name: 'To do lists',
+    component: ViewToDos
+  },
 ]
+
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
---
+++ frontend/src/views/ViewToDos.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="ViewToDos">
+      <h1>This page is for to do lists!</h1>
+    </div>
+  </template>
+
+<script>
+export default {
+  name: "ViewToDos"
+}
+</script>
