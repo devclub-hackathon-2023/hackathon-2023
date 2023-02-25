610582b2a1c4acb29cf2870d9fe0543b7b0d98ea
Caelan Myskiw <myskiwc@myumanitoba.ca>
2023-02-25 11:52:35 -0600

    Finished read_file

--- backend/fileIO.py
+++ backend/fileIO.py
@@ -3,19 +3,23 @@ import json
 #user will be user{username: "", pass: ""}
 def create_file(user: json):
     userDict = json.loads(user)
-    file = open(userDict['username'] + '.json', 'x')
+    file = open(userDict['username'] + '.json', 'w')
     file.write(user)
     file.close()       
 #end of write_file
 
-def read_file(user: json):
+def read_file(user: json) -> json:
     userDict = json.loads(user)
     try:
-        file = open(userDict)
+        file = open(userDict['username'] + '.json', 'r')
+        usersFile = file.read()
+        file.close()
+        return usersFile
     except FileNotFoundError:
-        pass
+        return -1
 #end of read_file
 
-thing = {"username": "John", "pass": "ThisIsMyPassword"}
-thing = json.dumps(thing)
-create_file(thing)
+john = {"username": "John", "pass": "ILikePasswords"}
+
+create_file(json.dumps(john))
+print(read_file(json.dumps(john)))
