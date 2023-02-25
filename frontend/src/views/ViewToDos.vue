60df021216f31f7c86cb9d814e1e2d868e9baf2f
kameron <khr675@gmail.com>
2023-02-25 14:29:54 -0600

    tasks fiel

--- frontend/src/views/ViewToDos.vue
+++ frontend/src/views/ViewToDos.vue
@@ -1,24 +1,36 @@
 <template>
+
     <div class= "ViewToDos">
       <h1>Your To Do List:</h1>
     </div>
+
     <div class="checklist">
-             <h1>Complete or Add to your list!</h1>
-             <br>
-             <form class="checklist" method="post">
-              <br> <br>
-                 <button type="button" id="addListButton" @click="onAddNew()">
-                  {{ addtxt }}
-            </button>
-            <br>
-            <p> </p><textarea id="comment" name="comments" rows="1" cols="20"></textarea>
-            </form>
-            <br><br>
-            <ul id="list"></ul>
-            <input type= "list" value = "addToList" id = "add">
-             
-  </div>
+      <h1>Complete or Add to your list!</h1>
+      <br>
+
+      <form class="checklist" method="post">
+
+        <br> <br>
+
+        <button type="button" id="addListButton" @click="onAddNew()">
+          {{ addtxt }}
+        </button>
+        <br>
+        <p> </p>
+        
+        <textarea id="comment" name="comments" rows="1" cols="20"></textarea>
+      </form>
+      <br><br>
+      <ul id="list"></ul>
+
+      <h1>Add new task to list:</h1>
+      <input type= "text" id="add" v-model="newTask.text">
+      <button value="add task" @click="onAddTaskClicked"> {{ addtxt }} </button>
+      {{ tasks }}
+              
+    </div>
   <div id = "textAdded">
+
   </div>
 
     
@@ -31,19 +43,29 @@ export default
   data() 
   {
     return {
+      tasks: [],
+      newTask: {
+        completed: false,
+        text: "",
+      },
       welcomeTxt: "Select/Create a To-Do-List:",
       addtxt : "Update List",
     }
   },
   methods: 
   {
-      onAddNew() 
-      {
-        document.getElementById("textAdded").innerText = document.getElementById("comment").value
-        var hello = document.getElementById("comment").value
-        console.log(hello)
-        console.log("Update list checked");
-      },
+    onAddNew() 
+    {
+      document.getElementById("textAdded").innerText = document.getElementById("comment").value
+      var hello = document.getElementById("comment").value
+      console.log(hello)
+      console.log("Update list checked");
+    },
+    onAddTaskClicked() {
+      console.log("new task:" + this.newTask)
+      this.tasks.push(this.newTask)
+      this.newTask = ""
+    }
   }
 }
 </script>
