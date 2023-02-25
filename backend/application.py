fd04cd5f30e72aa77c46c8c2bd5a5c1d4964aec2
Luke Wiebe <mail@lukewiebe.com>
2023-02-25 11:10:25 -0600

    basic working application.py

--- backend/application.py
+++ backend/application.py
@@ -8,5 +8,15 @@ app = Flask(__name__)
 @app.get("/")
 def hello_world():
     sample_dict = {"test": "123", "testing": "456"}
-    sample_dict = json.dump(sample_dict)
+    sample_dict = json.dumps(sample_dict)
     return sample_dict
+
+@app.post("/api/account/create_account")
+def create_account():
+    # username = request.get_json()["username"]
+    # password = request.get_json()["password"]
+    # session_cookie = # create cookie
+    return "woohoo this thing is working"
+
+# @app.get("/api/account/login")
+# def login():
