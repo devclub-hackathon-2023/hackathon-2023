364ecb9d6dd4f2e983c5cd0b59026fbd6477dbcc
Pamela Pena Reyes <pamelapr2501@gmail.com>
2023-02-25 10:46:51 -0600

    User points

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,8 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to= "/Lists">Lists</router-link>
+    <router-link to= "/Lists">Lists</router-link> |
+    <router-link to= "/Ptsview">Points</router-link>
   </nav>
   <router-view/>
  
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView'
+import PtsView from '../views/PtsView'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  
+  {
+    path: '/Ptsview',
+    name: 'points',
+    component: PtsView
   },
 ]
 
