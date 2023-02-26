19a94adc709d10e2f492fe4dbd3926a2678a0fb2
kameron <khr675@gmail.com>
2023-02-25 19:01:45 -0600

    wfasdfsa

--- frontend/src/components/taskItem.vue
+++ frontend/src/components/taskItem.vue
@@ -1,10 +1,5 @@
 <template>
-<<<<<<< HEAD
   <div v-if="!task.complete" class="taskContainer">
-=======
-
-  <div v-if="task.complete" class="taskContainer">
->>>>>>> 17f4340eb4ed9f2643dbfb6c6f073f56066c0c9d
     <span class="deleteIcon" @click= "onDeleteClicked"> [x] </span>
     <span class="taskText">{{ task.text }}</span>
 
